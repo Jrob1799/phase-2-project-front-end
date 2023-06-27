becfaf13562e72f9eda81455f93ff66d2efdd2ef
Jacob <jacobwroberts33@gmail.com>
2023-06-27 02:39:11 -0400

    add css to team builder

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Route path="/team-builder">
           <TeamBuilder />
         </Route>
-        <Route path="/">
+        <Route exact path="/">
           <Pokedex />
         </Route>
       </Switch>
