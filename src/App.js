5b0b828bd031d715d0017affcb5a35b08d1cd2b1
Jacob <jacobwroberts33@gmail.com>
2023-06-27 04:56:14 -0400

    import axios to app and use try/catch

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import Pokedex from './components/Pokedex';
 import TeamBuilder from './components/TeamBuilder';
 import NavBar from './components/NavBar';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   const [teams, setTeams] = useState([]);
 
-  const addTeam = (username, team) => {
-    setTeams([...teams, {username, team}]);
-  }
+  const addTeam = async (username, team) => {
+    try {
+      // Replace this URL with your server's endpoint for adding a team
+      const url = 'http://localhost:4000/teams';
+
+      const response = await axios.post(url, { username, team });
+
+      if (response.status === 200) {
+        console.log('Team successfully added');
+      } else {
+        console.error('Error adding team:', response);
+      }
+    } catch (error) {
+      console.error('Error adding team:', error);
+    }
+  };
 
   return (
     <Router>
