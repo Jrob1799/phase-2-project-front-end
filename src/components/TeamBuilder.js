7beb0b67b4affda19bde5cbc0b713194c59f8a04
Jacob <jacobwroberts33@gmail.com>
2023-06-27 03:28:53 -0400

    update TeamBuilder

--- src/components/TeamBuilder.js
+++ src/components/TeamBuilder.js
@@ -3,27 +3,27 @@ import PokemonForm from './PokemonForm';
 import SelectedPokemonCard from './SelectedPokemonCard';
 import axios from 'axios';
 
-const TeamBuilder = () => {
-    const [team, setTeam] = useState([]);
+const TeamBuilder = ({ addTeam }) => {
+  const [team, setTeam] = useState([]);
 
-    const handleFormSubmit = async (formData) => {
-        await axios.post('http://localhost:4000/teams', formData);
+  const handleFormSubmit = async (formData) => {
+    await axios.post('http://localhost:4000/teams', formData);
 
-        // Fetch data for each Pokemon in the team and set in state
-        const teamData = await Promise.all(formData.team.map(pokemonName => 
-            axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-        ));
-        setTeam(teamData.map(response => response.data));
-    };
+    // Fetch data for each Pokemon in the team and set in state
+    const teamData = await Promise.all(formData.team.map(pokemonName =>
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    ));
+    setTeam(teamData.map(response => response.data));
+  };
 
-    return (
-        <div className="team-builder">
-            <PokemonForm onSubmit={handleFormSubmit} />
-            {team.map(pokemon => (
-                <SelectedPokemonCard key={pokemon.name} pokemon={pokemon} />
-            ))}
-        </div>
-    );
+  return (
+    <div className="team-builder">
+      <PokemonForm onSubmit={handleFormSubmit} addTeam={addTeam} />
+      {team.map(pokemon => (
+        <SelectedPokemonCard key={pokemon.name} pokemon={pokemon} />
+      ))}
+    </div>
+  );
 };
 
 export default TeamBuilder;
