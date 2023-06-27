cc4b173af252b66c193889e1a7b58c402193a0e5
Jacob <jacobwroberts33@gmail.com>
2023-06-27 01:58:57 -0400

    update PokemonForm

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,55 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const PokemonForm = ({ addTeam }) => {
-  const [pokemonList, setPokemonList] = useState([]);
-  const [team, setTeam] = useState(Array(6).fill(''));
-  const [username, setUsername] = useState('');
+const PokemonForm = ({ onPokemonChange, onSubmit }) => {
+    const [pokemonList, setPokemonList] = useState([]);
+    const [team, setTeam] = useState(Array(6).fill(''));
+    const [username, setUsername] = useState('');
 
-  // Fetch Pokemon when component mounts
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        'https://pokeapi.co/api/v2/pokemon?limit=151' // First 151 Pokemon
-      );
-      setPokemonList(result.data.results);
+    useEffect(() => {
+        const fetchPokemon = async () => {
+            const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151');
+            setPokemonList(response.data.results);
+        };
+
+        fetchPokemon();
+    }, []);
+
+    const handleSelectChange = (index, event) => {
+        const newTeam = [...team];
+        newTeam[index] = event.target.value;
+        setTeam(newTeam);
+
+        if (onPokemonChange) {
+            onPokemonChange(event.target.value);
+        }
     };
-    fetchData();
-  }, []);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    addTeam(username, team);
-    setTeam(Array(6).fill(''));
-    setUsername('');
-  };
+    const handleInputChange = (event) => {
+        setUsername(event.target.value);
+    };
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
-  const handlePokemonChange = (index) => (event) => {
-    const newTeam = [...team];
-    newTeam[index] = event.target.value;
-    setTeam(newTeam);
-  };
+        if (onSubmit) {
+            onSubmit({ username, team });
+        }
+    };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={username} onChange={handleUsernameChange} placeholder="Enter username" required />
-      {[...Array(6)].map((_, index) => (
-        <select key={index} value={team[index]} onChange={handlePokemonChange(index)} required>
-          <option value="">--Choose a Pokemon--</option>
-          {pokemonList.map(pokemon => (
-            <option key={pokemon.name} value={pokemon.name}>
-              {pokemon.name}
-            </option>
-          ))}
-        </select>
-      ))}
-      <button type="submit">Add Team</button>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            {team.map((pokemon, index) => (
+                <select key={index} value={pokemon} onChange={event => handleSelectChange(index, event)}>
+                    <option value="">--Please choose a Pokemon--</option>
+                    {pokemonList.map(pokemon => (
+                        <option key={pokemon.name} value={pokemon.name}>
+                            {pokemon.name}
+                        </option>
+                    ))}
+                </select>
+            ))}
+            <input type="text" placeholder="Username" value={username} onChange={handleInputChange} />
+            <button type="submit">Submit Team</button>
+        </form>
+    );
 };
 
 export default PokemonForm;
