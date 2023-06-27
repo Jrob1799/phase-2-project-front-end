d3d330b248123d4f1e7781a3a09a24b9b24ce7f8
Jacob <jacobwroberts33@gmail.com>
2023-06-27 03:27:18 -0400

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Pokedex from './components/Pokedex';
 import TeamBuilder from './components/TeamBuilder';
@@ -6,12 +6,18 @@ import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
+  const [teams, setTeams] = useState([]);
+
+  const addTeam = (username, team) => {
+    setTeams([...teams, {username, team}]);
+  }
+
   return (
     <Router>
       <NavBar />
       <Switch>
         <Route path="/team-builder">
-          <TeamBuilder />
+          <TeamBuilder addTeam={addTeam} />
         </Route>
         <Route exact path="/">
           <Pokedex />
