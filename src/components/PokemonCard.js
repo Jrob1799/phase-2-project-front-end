157b0ac3c3ae1772cdf270f0fed1b2885aa713a2
Jacob <jacobwroberts33@gmail.com>
2023-06-27 00:02:03 -0400

    add class

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const PokemonCard = ({ pokemon }) => {
     return (
-        <div>
-            <h3>{pokemon.name}</h3>
-            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-            <p>Types: {pokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
+        <div className="pokemon-card">
+          <h3>{pokemon.name}</h3>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          <p>Types: {pokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
         </div>
-    );
+      );
 };
 
 export default PokemonCard;
